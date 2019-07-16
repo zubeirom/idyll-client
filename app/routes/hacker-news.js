1e504958755d385dc248e4079bd20948b098a28c
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-16 10:22:14 +0200

    hacker news added

--- app/routes/hacker-news.js
+++ app/routes/hacker-news.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+    ajax: service(),
+
+    async model() {
+        const res = await this.get('ajax').request('https://api.hnpwa.com/v0/news/1.json');
+        console.log(res);
+        return res;
+    }
 });
