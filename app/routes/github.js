d33f8269d89e88a369bd3f4626ec4c9a38cfc660
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-17 08:42:02 +0200

    add github api

--- app/routes/github.js
+++ app/routes/github.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+    ajax: service(),
+
+    async model() {
+        const res = await this.get('ajax').request('https://github-trending-api.now.sh/repositories?since=weekly');
+        return res
+    }
+
 });
