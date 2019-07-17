89e8b2b0284224da262b38349979a63ed7ed0a0d
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-17 06:11:55 +0200

    add reddit

--- app/routes/reddit.js
+++ app/routes/reddit.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import NewsAPI from 'newsapi'
+import { set } from '@ember/object';
 
 export default Route.extend({
+    ajax: service(),
+
+    async model() {
+        return {};
+    },
+
+    async afterModel(model) {
+        const newsapi = new NewsAPI('a6621af2728b4b689ba4f4c17255b292');
+        try {
+            newsapi.v2.everything({
+                sources: 'reddit-r-all',
+            }).then(response => {
+                const { articles } = response;
+                console.log(articles)
+                set(model, 'articles', articles);
+                set(model, 'categories', ['business', 'entertainment', 'health', 'science', 'sports', 'technology'])
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 });
