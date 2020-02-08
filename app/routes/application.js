6d34b153eac1a31c62d9dfe131f7d9a71fc164e8
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-02-08 04:58:25 +0100

    add hammer js

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,4 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  serviceWorkerUpdateNotify: service(),
-  
-  beforeModel() {
-    this.serviceWorkerUpdateNotify.on('update', () => {
-      window.location.reload();
-    });
-  }
 });
