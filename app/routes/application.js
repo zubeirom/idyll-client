eba9564857b6a829b7c469c09caf7fadc4e61600
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-02-08 04:08:25 +0100

    add update notifier

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  serviceWorkerUpdateNotify: service(),
+  
+  beforeModel() {
+    this.serviceWorkerUpdateNotify.on('update', () => {
+      window.location.reload();
+    });
+  }
 });
--- app/routes/news.js
+++ app/routes/news.js
@@ -1,27 +1,36 @@
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import NewsAPI from 'newsapi'
-import { set } from '@ember/object';
+import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
+import NewsAPI from "newsapi";
+import { set } from "@ember/object";
 
 export default Route.extend({
-    ajax: service(),
+  ajax: service(),
 
-    async model() {
-        return {};
-    },
+  async model() {
+    return {};
+  },
 
-    async afterModel(model) {
-        const newsapi = new NewsAPI('a6621af2728b4b689ba4f4c17255b292');
-        try {
-            newsapi.v2.topHeadlines({
-                language: 'en',
-            }).then(response => {
-                const { articles } = response;
-                set(model, 'articles', articles);
-                set(model, 'categories', ['business', 'entertainment', 'health', 'science', 'sports', 'technology'])
-            });
-        } catch (error) {
-            console.log(error);
-        }
+  async afterModel(model) {
+    const newsapi = new NewsAPI("a6621af2728b4b689ba4f4c17255b292");
+    try {
+      newsapi.v2
+        .topHeadlines({
+          language: "en"
+        })
+        .then(response => {
+          const { articles } = response;
+          set(model, "articles", articles);
+          set(model, "categories", [
+            "business",
+            "entertainment",
+            "health",
+            "science",
+            "sports",
+            "technology"
+          ]);
+        });
+    } catch (error) {
+      console.log(error);
     }
+  }
 });
