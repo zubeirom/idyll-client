1f4a47ac619c4575cd154a8541b13cba0c1ecac4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-02-08 05:42:31 +0100

    wip

--- app/routes/youtube.js
+++ app/routes/youtube.js
@@ -6,7 +6,7 @@ export default Route.extend({
 
     async model() {
         try {
-            const url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics%2C%20snippet&chart=mostPopular&maxResults=20&key=AIzaSyDVJHTDd344HrKnPc2-DEJNimT1luoqQWU'
+            const url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics%2C%20snippet&chart=mostPopular&maxResults=20&key=AIzaSyA2pCWAsYrEA7hGS0Bk1CbV3KUOPiI-JTs'
             const res = await this.get('ajax').request(url);
             return res.items;
         } catch (error) {
