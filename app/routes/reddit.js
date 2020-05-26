747edd3ceac7bf7bed29a16524bf429a3d41aadf
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-05-26 04:38:59 +0200

    wip

--- app/routes/reddit.js
+++ app/routes/reddit.js
@@ -12,7 +12,7 @@ export default Route.extend({
   async afterModel(model) {
     try {
       const key = "a6621af2728b4b689ba4f4c17255b292";
-      const url = `https://newsapi.org/v2/everything?sources=reddit-r-all&apiKey=${key}`
+      const url = `http://newsapi.org/v2/everything?sources=reddit-r-all&apiKey=${key}`
       const res = await this.get('ajax').request(url);
       set(model, "articles", res.articles);
 
